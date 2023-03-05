376823f93b1c10e45487d34791bafb0cde3973e0
Ezequiel Capaceta <ezequielcapaceta@zeki.local>
2023-03-05 12:42:14 -0800

    first commit

--- BST.py
+++ BST.py
@@ -1,4 +1,14 @@
 #Part 1: Create a BSTNode class
+class BSTNode:
+  def __init__(self, data = None, left = None, right = None):
+    self.data = data
+    self.left = left
+    self.right = right
+  def __str__(self):
+    return str(self.data)
+  def __repr__(self):
+    return str(self.data)
+
 
 #Part 2: Create a BST class
   #Part 3: Add functionality to your BST class
